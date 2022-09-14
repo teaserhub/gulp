c038c3bee34195b5e56fedee7f716aeaad2bb54c
Renat <adman.ufa@gmail.com>
2022-09-14 21:20:59 +0500

    Решил проблему с прерыванием gulp, добавил notify

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 const { series, src, dest, parallel, watch } = require("gulp");
 const htmlmin = require("gulp-html-minifier");
-const uglify = require('gulp-uglify-es').default;
+const uglify = require("gulp-uglify-es").default;
 const sass = require("gulp-sass")(require("sass"));
 const postcss = require("gulp-postcss");
 const autoprefixer = require("gulp-autoprefixer");
 const plumber = require("gulp-plumber"); // Отслеживание ошибок
-const sourcemaps = require("gulp-sourcemaps"); // Отслеживание ошибок
+const notify = require("gulp-notify");
+const sourcemaps = require("gulp-sourcemaps");
 const fileinclude = require("gulp-file-include");
 const rename = require("gulp-rename");
 const del = require("del");
@@ -13,11 +14,13 @@ const bs = require("browser-sync").create();
 
 // html
 const pages = () => {
-  return (src("src/*.html"))
+  return (
+    src("src/*.html")
       .pipe(fileinclude())
       // .pipe(htmlmin({collapseWhitespace: true}))
       .pipe(dest("www/"))
       .pipe(bs.stream())
+  );
 };
 exports.pages = pages;
 
@@ -25,7 +28,7 @@ exports.pages = pages;
 const scripts = () => {
   return src("src/js/script.js")
     .pipe(uglify())
-    .pipe(rename('script.min.js'))
+    .pipe(rename("script.min.js"))
     .pipe(dest("www/js/"))
     .pipe(bs.stream());
 };
@@ -35,7 +38,17 @@ exports.scripts = scripts;
 const styles = () => {
   return (
     src("src/scss/*.scss")
-      .pipe(plumber())
+      .pipe(
+        plumber({
+          errorHandler: notify.onError(function (err) {
+            return {
+              title: "Styles",
+              sound: false,
+              message: err.message,
+            };
+          }),
+        })
+      )
       .pipe(sourcemaps.init())
       .pipe(sass())
       .pipe(autoprefixer(["last 15 versions"]))
@@ -56,13 +69,13 @@ const server = () => {
 exports.server = server;
 
 const watchers = () => {
-  watch("src/scss/*.scss", styles);
+  watch("src/**/*.scss", styles);
   watch("src/**/*.html", pages);
 };
 
 const clean = () => {
-    return del('www/')
-}
+  return del("www/");
+};
 
 const build = series(clean, parallel(pages, scripts, styles));
 
